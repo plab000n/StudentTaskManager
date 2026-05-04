007de752f57a0bdfad005f0e55c344c25f3715a6
plab000n <pkplabon121@gmail.com>
2026-05-04 11:35:12 +0600

    Create Task class

---
+++ Task.java
@@ -0,0 +1,19 @@
+public class Task {
+    int id;
+    String title;
+    boolean isCompleted;
+
+    public Task(int id, String title) {
+        this.id = id;
+        this.title = title;
+        this.isCompleted = false;
+    }
+
+    public void markComplete() {
+        isCompleted = true;
+    }
+
+    public String toString() {
+        return id + ". " + title + " [" + (isCompleted ? "Done" : "Pending") + "]";
+    }
+}
